68e47339116358bb00dec90894b7fe1917c5ad23
arabelle873 <53376960+arabelle873@users.noreply.github.com>
2019-07-27 23:31:24 +0700

    tugas

---
+++ dia/dia.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<title>Tugas</title>
+<style>
+body {
+        background-image: url("1.jpg")
+} 
+ 
+</style>
+ 
+</head>
+ 
+<body><div class="content">
+	<header>
+		<h1 class="judul">Welcome To Website</h1>
+	</header>
+ 
+	<div class="menu">
+		<ul>
+			<li><a>HOME</a></li>
+			<li><a>TENTANG</a></li>
+			<li><a>TUTORIAL</a></li>
+			<li><a>Login</a></li>
+		</ul>
+	</div>
+
+	<div class="badan">
+        <p> Website ini adalah website simple yang dibuat dan belum memliki function apapun </p>
+ 
+	</div>
+</div>
+ 
+</body>
+</html>
